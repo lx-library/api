cd1d4d09a6621dc32692e5d92fc8e09d0ae301ad
dwidge <dwidge+gh@gmail.com>
2023-09-16 17:40:50 +0200

    Add withUser

---
+++ lib/types/AuthUser.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const AuthUser = z.object({
+  uid: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
+  fullName: z.string(),
+  email: z.string(),
+  grade: z.string(),
+  roleId: z.number(),
+  courses: z.null(),
+});
+export type AuthUser = z.infer<typeof AuthUser>;
